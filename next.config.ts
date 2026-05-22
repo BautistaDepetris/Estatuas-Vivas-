3ee026caf1ee6c2bf46e17aeaf8731e46aa0e991
Bautista <bautistadepetris13@gmail.com>
2026-05-22 16:57:40 -0300

    fix supabase image domain

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,12 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'sydurcrkrjfpukwtampi.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
         hostname: 'images.unsplash.com',
       },
     ],
