b02688fe1ead52140e1226a638d81e9fef11ad63
Bautista <bautistadepetris13@gmail.com>
2026-05-31 15:54:56 -0300

    fondo home nuevo terminaditp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function HomePage() {
         getLugaresPueblo(),
     ]);
     return (<main style={{
-            backgroundImage: "url('/assets/fondo-vertical-.png')",
+            backgroundImage: "url('/assets/casi final.png')",
             backgroundRepeat: 'repeat',
             backgroundSize: '512px 512px',
             minHeight: '100vh',
